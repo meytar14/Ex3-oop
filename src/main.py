6dfdc2c6ad974bb9218834a45938db1125b379a6
meytar14 <61780283+meytar14@users.noreply.github.com>
2021-01-09 13:06:24 +0200

    test

--- src/main.py
+++ src/main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+
 from Graph import Graph
 from GraphAlgo import GraphAlgo
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
    graph=Graph()
    # graph.add_node(1,(0.7,3,2))
